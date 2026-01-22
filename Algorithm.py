5f06731151b2271ba9f54a52b343a2cf21b88807
Ashish Mishra <ashish.aashu2004@gmail.com>
2026-01-22 08:04:52 +0530

    Done

---
+++ Algorithm.py
@@ -0,0 +1,57 @@
+'''Write a program to check if a number is even or odd(done)
+Create a python function that returns the square of a number 
+Write a python program to find the largest of three numbers (done)
+Write a python program to calculate the sum of all the elements in a list (done)
+Write a python program to reverse a string (done)
+Write a program to check if a given word is a palindrome'''
+
+'''num= int(input("Enter a number: "))
+if num % 2==0:
+    print("Even")
+else:
+    print("odd")'''
+
+
+'''a=int(input("Enter a number: "))
+b=int(input("Enter a number: "))
+c=int(input("Enter a number: "))
+if a>b & a>c:
+    print("a is largest number")
+elif b>a & b>c:
+    print("b is largest number")
+else: 
+    print("c is largest number")'''
+
+
+'''list=[1,2,4,8,9]
+s=0
+for i in list:
+    s=s+i
+print(s)'''
+
+
+'''a="Ashish"
+for i in range (len(a)-1,-1,-1):
+    print(a[i],end="")'''
+
+
+'''word = input("Enter a word: ")
+
+if word == word[::-1]:
+    print("Palindrome")
+else:
+    print("Not a Palindrome")'''
+
+
+'''def square(n):
+    return n * n
+
+print(square(8))'''
+
+
+
+
+
+
+
+
