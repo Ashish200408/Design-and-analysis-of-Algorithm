0c4014c4493bdfa26adee8bf7594a2445f068679
Ashish Mishra <ashish.aashu2004@gmail.com>
2026-01-29 08:57:10 +0530

    Done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,20 @@
-def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
-
-num = int(input("Enter a number: "))
-print("Factorial =", factorial(num))
+import itertools 
+import sys
+def travelling_salesman(graph,start):
+    n=len(graph)
+    vertices=[i for i in range(n) if i!=start]
+    min_cost=sys.maxsize
+    for perm in itertools.permutations(vertices):
+        cost=graph[start][perm[0]]
+    for i in range(len(perm)-1):
+        cost+=graph[perm[i]][perm[i+1]]
+        cost+=graph[perm[-1]][start]
+        min_cost=min(min_cost,cost)
+        return min_cost
+graph=[
+    [0,10,35,20],
+    [10,0,35,25],
+    [15,35,0,30],
+    [20,25,30,0]
+]
+print("Minimum cost:",travelling_salesman(graph,0))
